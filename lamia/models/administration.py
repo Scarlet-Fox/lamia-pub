adeed212902b77922c534d86e259248c1a303d76
Scarly-Cat <spuriousscarlet@gmail.com>
2019-01-21 13:46:05 -0800

    the last of the initial comments on models, also we should have a glossary bc theres a lot of jargon going on in here

--- lamia/models/administration.py
+++ lamia/models/administration.py
@@ -1,13 +1,22 @@
 from .. import db
 
-
-
-
 class Emoji(db.Model):
+    """A mapping of images, replacement text, and description text all of which
+    exists for the purpose of proliferating blob emojis and upside down smiley
+    faces throughout the fediverse.
+    """
     __tablename__ = 'emojis'
 
 
 class Setting(db.Model):
+    """A basic key and value storage for settings.
+    
+    Note: From a philosophical standpoint, settings should all be optional, and
+    built around non-essential functionality. The installation of Lamia should
+    require as little gymnastics as possible and should be, dare I say, Fun.
+    
+    TODO: figure out settings we may need here.
+    """
     __tablename__ = 'settings'
 
 
