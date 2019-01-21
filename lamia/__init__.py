d9515f06da6601d79c1ecbdb3b98cca095ecb734
Scarly-Cat <spuriousscarlet@gmail.com>
2019-01-20 18:47:30 -0800

    changed a few comments

--- lamia/__init__.py
+++ lamia/__init__.py
@@ -2,10 +2,13 @@ from starlette.applications import Starlette
 from starlette.config import Config
 from .middleware.gino import Gino
 
+# Included this code to change gino's logging level. This prevents some double
+# logging that was making my lose my mind with uvicorn's defaults.
 import logging
 logging.basicConfig()
 logging.getLogger('gino').setLevel(logging.WARN)
 
+# Initialize the app, including the database connection.
 db = Gino()
 config = Config('.env')
 app = Starlette(debug=config("DEBUG", cast=bool, default=False))
