6001331199835096dfebe55bbf92da30c69815b2
Scarly-Cat <spuriousscarlet@gmail.com>
2019-01-21 13:39:59 -0800

    comments for moderation models

--- lamia/__init__.py
+++ lamia/__init__.py
@@ -2,17 +2,23 @@ from starlette.applications import Starlette
 from starlette.config import Config
 from .middleware.gino import Gino
 
+
 # Included this code to change gino's logging level. This prevents some double
 # logging that was making my lose my mind with uvicorn's defaults.
 import logging
 logging.basicConfig()
 logging.getLogger('gino').setLevel(logging.WARN)
+
+
 # Initialize the app, including the database connection.
 db = Gino()
 config = Config('.env')
 app = Starlette(debug=config("DEBUG", cast=bool, default=False))
 db.init_app(app, config)
+
+
 # TODO: Setup redis here
+
 
 # There's probably a more graceful way to do this (a la blueprints)
 from .views import core
--- lamia/models/moderation.py
+++ lamia/models/moderation.py
@@ -1,55 +1,87 @@
-"""Models that pertain directly to the messy business of keeping an online
+"""Models that pertain directly to the messy business of keeping an online 
 environment safe from harmful intentions are in this module.
-
-
 """
 from .. import db
 
 
 class Import(db.Model):
-    """Followed, muted, and blocked actors can be imported from ActivityPub
+    """Followed, muted, and blocked actors can be imported from ActivityPub 
     files. 
     
-    These imports should not run automatically because of the potential
-    for abuse, particularly from "follow" actions. Instead, they are stored here
-    and can be selectively authorized to move forward by moderators.    
+    These imports should not run automatically because of the potential 
+    for abuse, particularly from "follow" actions. Instead, they are stored here 
+    and can be selectively authorized to move forward by moderators.
     """
     __tablename__ = 'imports'
     
     
 class Report(db.Model):
-    """Reports are a necessary part of any online social environment. They are
+    """Reports are a necessary part of any online social environment. They are 
     a way to flag local content for moderators on a local instance, and they 
-    can be created and sent to this instance from other instances.  
+    can be created and sent to this instance from other instances. 
     """
     __tablename__ = 'reports'
-    
-class ActorBlock(db.Model):
-    __tablename__ = 'actor_blocks'
-    
-    
-    
-class ActorMute(db.Model):
-    __tablename__ = 'actor_mutes'
-    
+
     
 class ActorCensor(db.Model):
+    """An actor censor is a light server to client moderation action.
+    
+    An actor censor is a created at the server level, and forces an actors' 
+    activities to either appear content warned or minimized.
+    """
     __tablename__ = 'actor_censors'
 
 
-class DomainBlock(db.Model):
-    __tablename__ = 'domain_blocks'
+class ActorMute(db.Model):
+    """An actor block is a moderate server to client moderation action.
     
+    An actor mute is a created at the server level, and either hides the actor's
+    activities from the federated timeline or minimizes them.
+    """
+    __tablename__ = 'actor_mutes'
 
-class DomainMute(db.Model):
-    __tablename__ = 'domain_mutes'
+
+class ActorBlock(db.Model):
+    """An actor block is a severe server to client moderation action.
+    
+    An actor block is a created at the server level, and probits the blocked 
+    actor from interacting with the instance. It also breaks follows between 
+    actors on this instance and the actor that is blocked.
+    """
+    __tablename__ = 'actor_blocks'
     
     
 class DomainCensor(db.Model):
+    """A domain censor is a light server to server moderation action. 
+    
+    An domain censor is a created at the server level, and forces all activities
+    from a specific domain to either appear content warned or minimized.
+    """
     __tablename__ = 'domain_censors'
+    
+    
+class DomainMute(db.Model):
+    """A domain mute is a moderate server to server moderation action. 
+    
+    A domain mute is a created at the server level, and either hides the domains'
+    activities from the federated timeline or minimizes them.
+    """
+    __tablename__ = 'domain_mutes'
+
+
+class DomainBlock(db.Model):
+    """A domain block is a severe server to client moderation action.
+    
+    A domain block is a created at the server level, and probits all actors on
+    a blocked instance from interacting with the local instance. It also breaks 
+    follows between actors on this instance and the instance that is blocked.
+    """
+    __tablename__ = 'domain_blocks'
     
 
 class DomainEmailBlock(db.Model):
+    """Email domain blocks prevent harmful registrations and registrations 
+    from spammers.
+    """
     __tablename__ = 'domain_email_block'
-    
     
