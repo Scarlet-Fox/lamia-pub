7fa3d5d3bf5485ae05ce0a4c51c5e8112e1afe7b
Scarly-Cat <spuriousscarlet@gmail.com>
2019-01-23 19:56:17 -0800

    jinja2 is now ready to do some science, tho wont need it for much? mebbe?

--- lamia/__init__.py
+++ lamia/__init__.py
@@ -1,6 +1,8 @@
 from starlette.applications import Starlette
 from starlette.config import Config
 from .middleware.gino import Gino
+import jinja2
+import inspect
 
 
 # Included this code to change gino's logging level. This prevents some double
@@ -16,6 +18,35 @@ config = Config('.env')
 app = Starlette(debug=config("DEBUG", cast=bool, default=False))
 db.init_app(app, config)
 
+
+# Setup a jinja2 env (https://www.starlette.io/templates/)
+def setup_jinja2(template_dirs):
+    @jinja2.contextfunction
+    def url_for(context, name, **path_params):
+        request = context['request']
+        return request.url_for(name, **path_params)
+
+    loader = jinja2.FileSystemLoader(template_dirs)
+    env = jinja2.Environment(loader=loader, autoescape=True)
+    env.globals['url_for'] = url_for
+    return env
+
+templates_dirs = ['templates']
+try:
+    # I feel like this may not be cool, but it uh works.
+    import lamia
+    # Damn, I love neat tricks, this looks up the lamia module path.
+    module_template_dir = inspect.getfile(lamia)
+    # The path includes __init__.py so you have to drop it.
+    templates_dirs.append(
+        '/'.join(module_template_dir.split("/")[:-1]+['templates'])
+    )
+except NameError:
+    # All is well, just use the ./templates folder
+    pass
+
+jinja = setup_jinja2(templates_dirs)
+    
 
 # TODO: Setup redis here
 
