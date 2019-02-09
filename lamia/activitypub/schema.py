0dbfe2359e05d3d6946b03c0c27e9199a32fb9b1
Scarly-Cat <spuriousscarlet@gmail.com>
2019-02-09 11:47:05 -0800

    made the schema classes more pythonic to work with

--- lamia/activitypub/schema.py
+++ lamia/activitypub/schema.py
@@ -13,16 +13,48 @@ Note: These classes speak JSON but they do so in the form of a Python
 dictionary.
 """
 import logging
+from typing import Any
 from lamia.activitypub.fields import FIELD_TYPE, FIELD_REQUIRED, FIELD_VALIDATION
 from lamia.activitypub.fields import ACTIVTY_FIELDS, OBJECT_FIELDS, ACTOR_FIELDS
 from lamia.activitypub.context import LAMIA_CONTEXT
 LOGGER = logging.getLogger('lamia')
 
 
+class SchemaValidationException(Exception):
+    """Schema validation exceptions are raised when a schema is badly formed
+    in accordance with the given meta data..
+    """
+
+
 class Schema:
     """The base class for all of our JSON schemata (what a lovely word)."""
 
-    def __init__(self, json_to_load: dict = None, fields: dict = None) -> None:
+    def __setattr__(self, name: str, value: Any) -> None:
+        """We overrride python's standard attribute setting method to provide
+        a prettier way to access the internal representation than accessing
+        the dictionary directly.
+
+        A benefit to taking this approach is that it allows for inline
+        validation of values that are being set.
+        """
+
+        # Only fields and representation are set directly everything
+        # else is assumed to be in the internal json ld representation.
+        if name in ('fields', 'representation'):
+            object.__setattr__(self, name, value)
+        else:
+            if self.validate_field(name, value):
+                self.representation[name] = value
+            else:
+                raise SchemaValidationException(
+                    f'invalid value {value} for {name}')
+
+    def __getattr__(self, name: str) -> Any:
+        return self.representation[name]
+
+    def __init__(self, json_to_load: dict, fields: dict = None) -> None:
+        """Setup the internal object."""
+
         if json_to_load:
             self.representation = json_to_load
         else:
@@ -37,7 +69,11 @@ class Schema:
         """
         self.representation = json_to_load
 
-    def to_json_ld(self) -> None:
+    def to_json_ld(self) -> dict:
+        """Returns a copy of the representation as it is."""
+        return self.representation.copy()
+
+    def to_lamia_json_ld(self) -> None:
         """Adds lamia's context to a copy of the representation and then
         returns.
         """
@@ -46,46 +82,77 @@ class Schema:
         json_ld['@context'] = LAMIA_CONTEXT
         return json_ld
 
-    def validate(self) -> bool:
-        """Validate the internal JSON representation.
+    def validate_field(self, field: str, value_to_test: Any = None) -> bool:
+        """If given a field name, this function verifies either the existing
+        internal value or the given value_to_test.
 
-        Returns True if valid. Otherwise, returns False.
+        It returns True when a field is Not Invalidated. This means that a
+        True return value is expected when a field doesn't exist yet and isn't
+        required (because a non-existant field can't invalidate).
         """
-        for field, meta in self.fields.items():
-            # Does our internal representation contain this key
-            field_in_representation = field in self.representation
+        # Does our internal representation contain this key
+        field_in_representation = field in self.representation
 
+        # If this field is not part of our fields metadata, allow it
+        try:
+            meta = self.fields[field]
+        except KeyError:
+            return True
+
+        # If we are performing a theoretical test, don't expect the field
+        # to have already been in the internal representation.
+        if value_to_test is None:
             # If we should have a field but we do not, then return false
             if meta[FIELD_REQUIRED] and not field_in_representation:
                 LOGGER.warning(f'required field {field} is not in schema')
                 return False
 
-            if field_in_representation:
-                # Is the internal value a valid type? If not, return false
+        # Set the local value to either the given value or the existing
+        # internal value.
+        if value_to_test is None:
+            # If we are validating a field that is in the fields metadata
+            # then set it if it exists, if it does not exist, then return
+            # true because it is not invalidating any tests if it doesn't
+            # exist.
+            try:
                 local_value = self.representation[field]
+            except KeyError:
+                return True
+        else:
+            local_value = value_to_test
 
-                valid_type = False
-                type_idx = None
-                for i, _type in enumerate(meta[FIELD_TYPE]):
-                    valid_type = isinstance(local_value, _type)
+        # Is the local value a valid type? If not, return false
+        valid_type = False
+        type_idx = None
+        for i, _type in enumerate(meta[FIELD_TYPE]):
+            valid_type = isinstance(local_value, _type)
 
-                    if valid_type:
-                        type_idx = i
-                        break
+            if valid_type:
+                type_idx = i
+                break
 
-                if not valid_type:
-                    LOGGER.warning(
-                        f'field {field} is not a valid type in schema')
-                    return False
+        if not valid_type:
+            LOGGER.warning(f'field {field} is not a valid type in schema')
+            return False
 
-                # Check the validation function associated with the type
-                validation_function = meta[FIELD_VALIDATION][type_idx]
+        # Check the validation function associated with the type
+        validation_function = meta[FIELD_VALIDATION][type_idx]
 
-                if validation_function:
-                    if not validation_function(local_value):
-                        LOGGER.warning(
-                            f'field {field} failed validation in schema')
-                        return False
+        if validation_function:
+            if not validation_function(local_value):
+                LOGGER.warning(f'field {field} failed validation in schema')
+                return False
+
+        return True
+
+    def validate(self) -> bool:
+        """Validate the internal JSON representation.
+
+        Returns True if valid. Otherwise, returns False.
+        """
+        for field in self.fields.keys():
+            if not self.validate_field(field):
+                return False
 
         return True
 
