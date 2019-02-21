31c9f1261eda1207be88b64a442d0db8ff5bae0b
Conner Bondurant <conner.bondurant@triptych.me>
2019-02-20 21:24:29 -0500

    Merge most scripts into new click tools set

--- scripts/babel-init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pybabel init -i lamia/locales/lamia.pot -l en -d lamia/locales/ -D lamia
--- scripts/babel-update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pybabel update -i lamia/locales/lamia.pot -l en -d lamia/locales/ -D lamia
